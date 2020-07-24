6ce2fc83b57156047219b6041d40d7610fe5e125
Shreya-P-98 <68474577+Shreya-P-98@users.noreply.github.com>
2020-07-24 21:08:22 +0530

    Add files via upload

---
+++ File2OutputStreamUtil.java
@@ -0,0 +1,32 @@
+package com.montran.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class File2OutputStreamUtil {
+	private OutputStream outputStream;
+
+	public void writeDataIntoFile(File file, byte[] data) {
+		try {
+			outputStream = new FileOutputStream(file); //to connect
+			outputStream.write(data); //to write
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException :: ");
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IOException");
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				outputStream.close();
+			} catch (IOException e) {
+				System.out.println("IOException");
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+}
